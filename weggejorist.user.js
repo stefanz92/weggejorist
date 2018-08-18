c93a2c0e7975cfedbb5d2b21274c0d49514e22b7
stefanz92 <stefanzijlstra92@gmail.com>
2018-08-18 22:20:19 +0200

    version update

--- weggejorist.user.js
+++ weggejorist.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         weggejorist
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  try to take over the world!
 // @author       You
 // @grant        GM_xmlhttpRequest
